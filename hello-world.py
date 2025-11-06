674a8d87eaa82e2b6aecd7611ac2a41775eda5d7
Smee0 <piotrg413@gmail.com>
2025-11-06 00:21:29 -0600

    added random greeting generator functino

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,19 @@
 print("Hello World!")
 print("Learning about commits today!")
 print("You can do the same with 10 lines of code as you can with 100 lines of codes - Mr. G")
+
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+print(get_random_greeting())
+print("Learning about branches today!")
