6aa1e4f0107e66507861a92b0018ff6c0ff8862d
Smee0 <piotrg413@gmail.com>
2025-11-05 23:57:32 -0600

    added a third line of code

---
+++ eggboy.py
@@ -0,0 +1,1 @@
+print("hello my name is smee and this is my first python script")
---
+++ hello-world.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+print("Learning about commits today!")
+print("You can do the same with 10 lines of code as you can with 100 lines of codes - Mr. G")
